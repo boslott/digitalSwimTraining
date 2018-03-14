d451845f8139f73e9c19a33c4a946785f6f1009d
= <=>
2018-03-14 15:12:12 -0400

    add jumbotron sub navigation bar

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import fontawesome from '@fortawesome/fontawesome';
 import brands from '@fortawesome/fontawesome-free-brands';
-import { faHome, faInfoCircle, faListUl, faEnvelope } from '@fortawesome/fontawesome-free-solid'
+import { faHome, faInfoCircle, faListUl, faEnvelope, faPlayCircle } from '@fortawesome/fontawesome-free-solid'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './include/bootstrap';
 
 // Font Awesome icons library
-fontawesome.library.add(brands, faHome, faInfoCircle, faListUl, faEnvelope);
+fontawesome.library.add(brands, faHome, faInfoCircle, faListUl, faEnvelope, faPlayCircle);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
