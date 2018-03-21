0e4aade83b0b945ad4b6fd015ca1566ff90e2506
= <=>
2018-03-21 16:50:00 -0400

    add redux store to all courses page

--- client/src/Pages/Courses.js
+++ client/src/Pages/Courses.js
@@ -1,50 +1,41 @@
 // Packages
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 // Components
 import Nav from '../Components/Nav/Nav';
 import Footer from '../Components/Footer/Footer';
 import CourseCard from '../Components/CourseCard/CourseCard';
-import API from '../utils/API';
+import { getCourses } from '../actions';
 
 class Courses extends Component {
 
-  state = {
-    courses: [],
-    isLoading: true,
-  }
+  // state = {
+  //   courses: [],
+  //   isLoading: true,
+  // }
 
   componentWillMount() {
-    this.getCourses();
-  }
-
-  getCourses = async () => {
-    await API.getCourses()
-      .then(courses => {
-          let loadedCourses = [];
-          courses.data.map(course => (
-          loadedCourses.push(course)
-        ));
-        this.setState({ courses: loadedCourses, isLoading: false });
-      })
-      .catch(err => console.log(err));
+    this.props.getCourses();
   }
 
   render() {
+    const { courses } = this.props;
     return (
-      !this.state.isLoading &&
+      // !this.state.isLoading &&
       <div className='page'> 
         <Nav location={this.props.location} userAuth={this.props.isAuthenticated} />
         <div className='container my-5'>
           <div className='row flex justify-around relative'>
             <CourseCard
-              course={this.state.courses[0]}
+              course={courses[0]}
             />
             <CourseCard
-              course={this.state.courses[1]}
+              course={courses[1]}
             />
             <CourseCard
-              course={this.state.courses[2]}
+              course={courses[2]}
             />
           </div>
         </div>
@@ -54,4 +45,14 @@ class Courses extends Component {
   }
 } 
 
-export default Courses;
+const mapStateToProps = state => ({
+  courses: state.courses.courses,
+  coursesLoaded: state.courses.coursesLoaded,
+  coursesLoadedAt: state.courses.coursesLoadedAt,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getCourses,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Courses);
